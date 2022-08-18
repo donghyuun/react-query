db0d70fd9d537bb760ca954029cfd65fb50af4f3
DongHyun <bomul0524@gmail.com>
2022-08-19 08:26:01 +0900

    Handling Mutation Response, post 이후 변경된 데이터를 새로 불러올때, 따로 다시 fetch 할 필요없이 mutate에 사용된  useMutation 내에서 간단하게 다시 불러올 수 있다

--- src/exam_third/hooks/useSuperHerosData.js
+++ src/exam_third/hooks/useSuperHerosData.js
@@ -1,19 +1,20 @@
-import { useQuery, useMutation } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import axios from "axios";
+import { queryByLabelText } from "@testing-library/react";
 
 const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 
 const addSuperHero = (hero) => {
-  return axios.post('http://localhost:4000/superheroes',hero)
-}
+  return axios.post("http://localhost:4000/superheroes", hero);
+};
 
 export const useSuperHerosData = (onSuccess, onError) => {
   return useQuery("super-heroes", fetchSuperHeroes, {
     onSuccess,
     onError,
-    enabled:false,
+    enabled: false,
     // select: (data) => {
     //   const superHeroNames = data.data.map((hero) => hero.name);
     //   return superHeroNames;
@@ -22,5 +23,17 @@ export const useSuperHerosData = (onSuccess, onError) => {
 };
 
 export const useAddSuperHeroData = () => {
-  return useMutation(addSuperHero)//addSuperHero 는 백엔드로 post 할 함수이다.
-}
+  const queryClient = useQueryClient();
+  return useMutation(addSuperHero, {
+    onSuccess: (data) => {
+      //queryClient.invalidateQueries("super-heroes");
+      queryClient.setQueriesData('super-heroes', (oldQueryData) => {
+        //데이터를 post 하고, 바뀐 데이터를 얻기위해 새로운 request 를 하지 않아도 mutate 할때 바뀐 데이터를 동시에 가져올 수 있다.
+        return{
+          ...oldQueryData,
+          data: [...oldQueryData.data, data.data]
+        }
+      })
+    },
+  }); //addSuperHero 는 백엔드로 post 할 함수이다.
+};
